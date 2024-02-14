79611fa03f7f8f33925d217bd752d45e9ecf90d1
Mohammad suhail <skhan282290@gmail.com>
2024-02-14 12:02:27 +0530

    first comment after development

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: [
+      "upload.wikimedia.org",
+      "images.unsplash.com",
+      "mango.blender.org",
+      "download.blender.org",
+    ],
+  },
 };
 
 export default nextConfig;
